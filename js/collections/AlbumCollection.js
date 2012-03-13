a7365951061f322cb868dfd1a821c63208f9e3d9
Martin <cedeskog@gmail.com>
2012-03-13 16:33:04 +0100

    Fixed remove artist. Validation on addAlbumfield. Order albumTitle and artistName and added more style.

--- js/collections/AlbumCollection.js
+++ js/collections/AlbumCollection.js
@@ -6,14 +6,17 @@ define([
 	function( Backbone, AlbumModel ) {
 	
     	var AlbumCollection = Backbone.Collection.extend({
-    	   
     		model : AlbumModel,
     
     		localStorage : new Store("Albums"),
     
     		initialize : function() {
     			this.fetch();
-    		}
+    		},
+
+            comparator : function( model ) {
+                return model.get( 'albumTitle' );
+            }
     	});
     
     	return AlbumCollection;	
